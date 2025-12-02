df866931a6ac5ee1dbb749e515109ddc861a96cd
Zulfahmi <rizki.zulfahmi2018@gmail.com>
2025-12-02 16:10:53 +0700

    update: rename table api_tokens to auth_token

--- database/migrations/2025_12_02_090543_create_auth_tokens_table.php
+++ database/migrations/2025_12_02_090543_create_auth_tokens_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('api_tokens', function (Blueprint $table) {
+        Schema::create('auth_token', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')
                 ->constrained('users')
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('api_tokens');
+        Schema::dropIfExists('auth_token');
     }
 };
